e110318c69b5570f4301ad5b6c8e15ff5b972c8b
damianfurmanczyk <DamianFurmanczykGM@gmail.com>
2018-06-08 16:39:06 +0200

    adjusting for heroku deployment

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ require("dotenv/config");
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MLABS);
 
-mongoose.connection.on("error", err => {
-  console.error(`☠☠☠☠☠
+mongoose
+  .connection
+  .on("error", err => {
+    console.error(`☠☠☠☠☠
         ☠☠☠☠☠
         ☠☠☠☠☠error occured during connecting to db: \n >${err.message}
         ☠☠☠☠☠
         ☠☠☠☠☠
         ☠☠☠☠☠`);
-});
+  });
 
 require("./models/user");
 require("./models/post");
 
 const app = require("./appConfig");
 
-const server = app.listen(4200, () => {
+const server = app.listen(process.env.PORT || 3001, () => {
   console.log(`⏩ PORT  ${server.address().port} ⏪`);
   console.log(`⏩ PORT  ${server.address().port} ⏪`);
   console.log(`⏩ PORT  ${server.address().port} ⏪`);
